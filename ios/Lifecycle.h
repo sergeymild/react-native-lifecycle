3b7f147c0a4a8b94cd8c2ea4b5733cf3a50bf800
SergeyMild <>
2026-06-07 07:42:23 +0300

    fix: migrate to RN 0.84 — use install_modules_dependencies, drop old arch codegen deps

--- ios/Lifecycle.h
+++ ios/Lifecycle.h
@@ -1,12 +1,6 @@
 
-#ifdef RCT_NEW_ARCH_ENABLED
-#import "RNLifecycleSpec.h"
-
-@interface Lifecycle : NSObject <NativeLifecycleSpec>
-#else
 #import <React/RCTBridgeModule.h>
 
 @interface Lifecycle : NSObject <RCTBridgeModule>
-#endif
 
 @end
